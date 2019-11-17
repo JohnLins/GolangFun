d5e3ac5d85ef9ae3bdc6484325ac983c36d8d1d1
John Lins <46113320+JohnLins@users.noreply.github.com>
2019-11-16 21:37:48 -0800

    Update variables.go

--- Variables/variables.go
+++ Variables/variables.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 //Var block
+
 var (
 	fname string = "John"
 	age int = 15
@@ -44,3 +45,22 @@ func main() {
 
 	fmt.Println(stuff)
 }
+
+//ALL TYPES
+/*
+bool
+
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // alias for uint8
+
+rune // alias for int32
+     // represents a Unicode code point
+
+float32 float64
+
+complex64 complex128
+*/
